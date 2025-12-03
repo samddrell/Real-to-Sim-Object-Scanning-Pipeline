324a944356cee24d7d34db95d257b064a43df397
Samuel Drell <iwn18@txstate.edu>
2025-12-03 00:31:12 -0600

    Creates Test_3.py file - manually written

---
+++ scripts/test_3.py
@@ -0,0 +1,9 @@
+from isaacsim import SimulationApp
+simulation_app = SimulationApp({"headless": True})
+
+import omni.replicator.core as rep
+from pathlib import Path
+import os
+
+import shutil
+import time
