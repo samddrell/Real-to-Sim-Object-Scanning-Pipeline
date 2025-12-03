01f35a62bee0156b82f9fd2dc7a880b534358c1b
Samuel Drell <iwn18@txstate.edu>
2025-12-03 16:47:39 -0600

    Adds header comment to instant_ngp_test.py

---
+++ scripts/test_scripts/instant_ngp_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+End-to-end pipeline:
+
+1) images -> COLMAP + transforms.json (via colmap2nerf.py)
+2) transforms.json -> trained NeRF + mesh.obj (via scripts/run.py)
+3) mesh.obj -> mesh.usd (via headless Blender)
+
+"""
