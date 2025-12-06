89aa4ec32b8b2f61538afc93fcdbb8f5102abc0a
Samuel Drell <iwn18@txstate.edu>
2025-12-06 11:10:24 -0600

    Fixes outdated colmap->nerf command

--- scripts/third_party/colmap2nerf_compat.py
+++ scripts/third_party/colmap2nerf_compat.py
@@ -121,7 +121,7 @@ def run_colmap(args):
 	if os.path.exists(db):
 		os.remove(db)
 	do_system(f"{colmap_binary} feature_extractor --ImageReader.camera_model {args.colmap_camera_model} --ImageReader.camera_params \"{args.colmap_camera_params}\" --SiftExtraction.estimate_affine_shape=true --SiftExtraction.domain_size_pooling=true --ImageReader.single_camera 1 --database_path {db} --image_path {images}")
-	match_cmd = f"{colmap_binary} {args.colmap_matcher}_matcher --SiftMatching.guided_matching=true --database_path {db}"
+	match_cmd = f"{colmap_binary} {args.colmap_matcher}_matcher --database_path {db}"
 	if args.vocab_path:
 		match_cmd += f" --VocabTreeMatching.vocab_tree_path {args.vocab_path}"
 	do_system(match_cmd)
